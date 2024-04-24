b3a61cce96339d384930d8980960e698af144f17
sadiik1975 <sadiikbeen@gmail.com>
2024-04-24 16:01:09 -0400

    adding update

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 function addingEventListener() {
-}
+    // Inside the addingEventListener() function
+  
+    // Step 1: Get the element by its ID
+    const input = document.getElementById('button');
+  
+    // Step 2: Define the function to be called when the event occurs
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+  
+    // Step 3: Add event listener to the element
+    input.addEventListener('click', clickAlert);
+  }
+  
